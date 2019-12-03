f30a471996b09e8a297632ec5206724349084858
aperuchena <aimarperuchena@gmail.com>
2019-12-03 11:16:19 +0100

    cambiar estilos de edit

--- resources/views/user_edit.blade.php
+++ resources/views/user_edit.blade.php
@@ -66,40 +66,49 @@
 
 
 
-    <div class="div_usuario_editar">
+<section class="edit_user">
+	<div class="card">
+  	<div class="card-body">
+    	<h5 class="card-title">Perfil de usuario</h5>
+    	
+        	<div class="div_usuario_editar">
+    
+            	<form class="contact-form" action="{{route('user.update')}}" method="post">
+                	@csrf
+                	<div class="form-group">
+                	<span>{{ __('multi.pnombre') }}</span>
 
-    <form class="contact-form" action="{{route('user.update')}}" method="post">
-        @csrf
-        <div class="form-group">
-<span>{{ __('multi.pnombre') }}</span>
-            <input id="name" type="text" style="border:solid black 1px;" class="form-control" name="name" value=" {{ Auth::user()->name }}">
-            @if($errors->has('name'))
-            <div class="error">{{ $errors->first('name') }}</div>
-            @endif
-        </div>
+                    	<input id="name" type="text" class="form_edit_user" name="name" value="{{ Auth::user()->name }}">
+                    	@if($errors->has('name'))
+                    	<div class="error">{{ $errors->first('name') }}</div>
+                    	@endif
+                	</div>
 
-        <div class="form-group">
-        <span>{{ __('multi.correo') }}</span>
+                	<div class="form-group">
+                	<span>{{ __('multi.correo') }}</span>
 
-            <input id="email" type="email" class="form-control" name="email" value=" {{ Auth::user()->email }}">
-            @if($errors->has('email'))
-            <div class="error">{{ $errors->first('email') }}</div>
-            @endif
-        </div>
-        <div class="form-group">
-        <span>{{ __('multi.pass') }}</span>
+                    	<input id="email" type="email" class="form_edit_user" name="email" value="{{ Auth::user()->email }}">
+                    	@if($errors->has('email'))
+                    	<div class="error">{{ $errors->first('email') }}</div>
+                    	@endif
+                	</div>
+                	<div class="form-group">
+                	<span>{{ __('multi.pass') }}</span>
 
-            <input id="password" type="password" class="form-control" name="password" placeholder="{{ __('multi.pass') }}">
-            @if($errors->has('password'))
-            <div class="error">{{ $errors->first('password') }}</div>
-            @endif
-        </div>
-        <input type="hidden" name="id" value=" {{ Auth::user()->id }}">
+                    	<input id="password" type="password" class="form_edit_user" name="password" placeholder="Password...">
+                    	@if($errors->has('password'))
+                    	<div class="error">{{ $errors->first('password') }}</div>
+                    	@endif
+                	</div>
+                    <input type="hidden" name="id" value=" {{ Auth::user()->id }}">
 
-        <button id="enviar" type="submit" class="btn btn-primary" name="Send">{{ __('multi.enviar') }}</button>
-    </form>
+                	<button id="enviar" type="submit" class="btn btn-primary" name="Send">{{ __('multi.enviar') }}</button>
+            	</form>
+    	
+  	</div>
+	</div>
+</section>
 
-    </div>
     
 
 
