53d3596426bd558dd70fa00edebf2603897e48e9
aperuchena <aimarperuchena@gmail.com>
2019-12-03 11:07:31 +0100

    validacion correccion

--- public/assets/js/validacion_editar.js
+++ public/assets/js/validacion_editar.js
@@ -1,16 +1,45 @@
+const validarName = (name) => {
+    if (name.length > 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+const validarEmail = (email) => {
+    if (/\S+@\S+\.\S+/.test(email)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const validarPassword = (password) => {
+    if (password.length >= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 $(document).ready(function() {
 
     let name;
     let email;
     let password;
     let enviar;
-    let bol_email = false;
+    let bol_email = true;
     let bol_pass = false;
-    let bol_name = false;
+    let bol_name = true;
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
     enviar = document.getElementById("enviar");
     name = document.getElementById("name").value;
+    validarName(name);
+    validarEmail(email);
+    validarPassword(password);
     enviar.disabled = true;
 
     $('#email').on('input', function(e) {
@@ -33,36 +62,16 @@ $(document).ready(function() {
 
 
 
-    const validarName = (name) => {
-        if (name.length > 2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-
-    const validarEmail = (email) => {
-        if (/\S+@\S+\.\S+/.test(email)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    const validarPassword = (password) => {
-        if (password.length >= 10) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     const validar = () => {
         if (bol_email === true && bol_pass === true && bol_name === true) {
             enviar.disabled = false;
         } else {
             enviar.disabled = true;
+            console.log('Name: ' + bol_name + " Email: " + bol_email + " Pass: " + bol_pass)
         }
     }
+
+
 })
--- resources/views/edit_user.blade.php
+++ resources/views/edit_user.blade.php
@@ -6,17 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/carousel.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="icon" type="img/png" href="assets\img\logo_alpha.png">
+
+ 
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="icon" type="img/png" href="{{ asset('assets\img\logo_alpha.png') }}">
 </head>
 
 <body>
 <section class="header">
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-      <a href="#top"><img src="assets\img\logo_alpha_white.png" width="50" height="50" alt="logo"></a>
+ 
+      <a href="#top"><img src="{{ asset('assets\img\logo_alpha_white.png') }}" width="50" height="50" alt="logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
       </button>
@@ -50,13 +53,13 @@
 
               
                 <li class="nav-item">
-                    <a href="{{ url('locale/es') }}"><img class="banderas" src="assets/img/espania.ico"></a>
+                    <a href="{{ url('locale/es') }}"><img class="banderas" src="{{ asset('assets/img/espania.ico') }}"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('locale/en') }}"><img class="banderas" src="assets/img/reino_unido.ico"></a>
+                    <a href="{{ url('locale/en') }}"><img class="banderas" src="{{ asset('assets/img/reino_unido.ico') }}"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('locale/eus') }}"><img class="banderas" src="assets/img/pais_vasco.ico"></a>
+                    <a href="{{ url('locale/eus') }}"><img class="banderas" src="{{ asset('assets/img/pais_vasco.ico') }}"></a>
                 </li>
 
           </ul>
@@ -73,7 +76,7 @@
             <div class="form-group">
             <span>{{ __('multi.pnombre') }}</span>
 
-                <input id="nombre" type="text" class="form-control" name="name" value=" {{ $usuario->name }}">
+                <input id="name" type="text" class="form-control" name="name" value=" {{ $usuario->name }}">
                 @if($errors->has('name'))
                 <div class="error">{{ $errors->first('name') }}</div>
                 @endif
@@ -111,13 +114,14 @@
     
 
 
-    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <script src="assets/js/jquery.backstretch.min.js"></script>
-    <script src="assets/js/validacion_editar.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/scripts.js"></script>
+    <script src="{{ asset('assets/js/jquery.backstretch.min.js') }}"></script>
+    <script src="{{ asset('assets/js/validacion_editar.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/scripts.js') }}"></script>
+
 </body>
 
 </html>
