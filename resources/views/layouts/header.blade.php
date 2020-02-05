6eb66fa81e7b6f276cb5090ebd4339b975cd5c24
aperuchena <aimarperuchena@gmail.com>
2020-02-05 22:24:07 +0100

    quitar header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -30,13 +30,13 @@
               </li>
               @endif
               <!-- ------------------------Mensajes------------------------- -->
-              <li class="nav-item dropdown no-arrow mx-1">
+              <!-- <li class="nav-item dropdown no-arrow mx-1">
                 <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="fas fa-envelope fa-fw"></i>
-                  <!-- Counter - Messages -->
+                 
                   <span class="badge badge-danger badge-counter">7</span>
                 </a>
-                <!-- Dropdown - Messages -->
+               
                 <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
                   <h6 class="dropdown-header text-white">
                     Message Center
@@ -83,15 +83,15 @@
                   </a>
                   <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
                 </div>
-              </li>
+              </li> -->
               <!-- -------------------------Alertas------------------------- -->
-              <li class="nav-item dropdown no-arrow mx-1">
+             <!--  <li class="nav-item dropdown no-arrow mx-1">
                 <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="fas fa-bell fa-fw"></i>
-                  <!-- Counter - Alerts -->
+                 
                   <span class="badge badge-danger badge-counter">3+</span>
                 </a>
-                <!-- Dropdown - Alerts -->
+              
                 <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
                   <h6 class="dropdown-header">
                     Alerts Center
@@ -131,7 +131,7 @@
                   </a>
                   <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
                 </div>
-              </li>
+              </li> -->
               <!-- -------------------Perfil del usuario-------------------- -->
               <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
