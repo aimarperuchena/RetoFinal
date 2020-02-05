c9e15d10af19699168da5ab35cb689f1c265bd4f
Kapkam <adrinro@gmail.com>
2020-02-05 19:32:02 +0100

    Fix systax error

--- app/Http/Controllers/SociedadController.php
+++ app/Http/Controllers/SociedadController.php
@@ -97,7 +97,7 @@ class SociedadController extends Controller
       $sociedad = Sociedad::find($sociedad_id);
       // $mesas = DB::select('select * from mesa where mesa.sociedad_id = '.$sociedad->id.' and id not in(select mesa_id from mesa_reserva where reserva_id in(select id from reserva where fecha like "'.$newDate.'" and sociedad_id='.$sociedad_id.' and tipo_id = '.$request->tipo.'))');
       $mesas = Mesa::where('sociedad_id',$sociedad->id)->get();
-      $mensaje = 'La capacidad de la mesa no soporta la cantidad de personas introducidas.'
+      $mensaje = 'La capacidad de la mesa no soporta la cantidad de personas introducidas.';
       return view('layouts.user.Reservas.index')->with('mesas',$mesas)->with('tipo',$tipo)->with('fecha',$fecha)->with('sociedad',$sociedad)->with('mensaje',$mensaje);
     }
   }
