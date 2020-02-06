9c465d45278b79d932c59c6e6a940c9e78044654
aperuchena <aimarperuchena@gmail.com>
2020-02-06 11:29:42 +0100

    prueba estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,16 +13,16 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
-    <link href="{{secure_asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{secure_asset('assets/css/blog-post.css')}}" rel="stylesheet">
-    <link rel="icon" type="img/png" href="{{secure_asset('assets\img\logo_alpha.png')}}">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/blog-post.css" rel="stylesheet">
+    <link rel="icon" type="img/png" href="/assets/img/logo_alpha.png">
 </head>
 <body id="page-top">
   @include('layouts.header')
   @yield('content')
-  <script src="{{ secure_asset('assets/js/jquery-3.4.1.min.js')}}"></script>
-    <script src="{{ secure_asset('assets/js/popper.min.js')}}"></script>
-    <script src="{{ secure_asset('assets/js/bootstrap.min.js')}}"></script>
+  <script src="/assets/js/jquery-3.4.1.min.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
