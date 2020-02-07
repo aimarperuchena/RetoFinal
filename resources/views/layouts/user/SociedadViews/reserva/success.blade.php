4c26d8e86d413ef0c84cc915a1fb585f9048e40e
aperuchena <aimarperuchena@gmail.com>
2020-02-07 09:19:27 +0100

    traducciones succes

--- resources/views/layouts/user/SociedadViews/reserva/success.blade.php
+++ resources/views/layouts/user/SociedadViews/reserva/success.blade.php
@@ -5,10 +5,10 @@
 <div class="container" style="height: 100vh;">
   <div class="jumbotron">
     <h1 class="display-4">{{ __('multi.exito') }}</h1>
-    <p class="lead">{{ __('multi.enorabuena') }} {{$fecha}}, {{ __('multi.enorabuena2') }} {{$mesa}}, {{ __('multi.para') }} {{$personas}} {{ __('multi.personas') }}.</p>
+    <p class="lead">{{ __('multi.enhorabuena') }} {{$fecha}}, {{ __('multi.enhorabuena2') }} {{$mesa}}, {{ __('multi.para') }} {{$personas}} {{ __('multi.personas') }}.</p>
     <hr class="my-4">
     <p>{{ __('multi.volver') }}</p>
-    <a class="btn btn-primary btn-lg" href="{{ route('usuario.listado') }}" role="button">{{ __('multi.volver2') }}</a>
+    <a class="btn btn-primary btn-lg" href="{{ route('usuario.listado') }}" role="button">{{ __('multi.volverSolo') }}</a>
   </div>
 </div>
 
